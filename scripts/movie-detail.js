1b97c687738851ded1933ff092c145bd4d8737e9
yheseul <kangyheseul@naver.com>
2025-03-20 09:38:59 +0900

    feat: 리뷰 CRUD API 적용

--- scripts/movie-detail.js
+++ scripts/movie-detail.js
@@ -1,8 +1,11 @@
 let movieId = null;
+let userId = null;
+let rateValue;
 
 document.addEventListener("DOMContentLoaded", () => {
   const urlParams = new URLSearchParams(window.location.search);
-  movieId = urlParams.get("id");
+  movieId = urlParams.get("movieId");
+  userId = urlParams.get("userId");
 
   if (!movieId) {
     window.location.href = "main.html";
@@ -10,7 +13,6 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   fetchMovieDetails(movieId);
-
   displayReviews(movieId);
 });
 
@@ -22,18 +24,165 @@ document
       setTimeout(() => {
         this.style.transform = "scale(1)";
       }, 200);
+      const forValue = star.getAttribute("for");
+      rateValue = forValue[forValue.length - 1];
     });
   });
 
 const fetchMovieDetails = async (movieId) => {
   try {
-    const response = await axios.get(
-      `http://localhost:8080/api/movies/now-playing/${movieId}`,
-      { withCredentials: true }
-    );
+    const response = await axios.get(`${MOVIE_API_URL}/${movieId}`);
     displayMovieDetails(response.data);
   } catch (error) {
     console.error("영화 데이터를 가져오는 중 오류 발생:", error);
+  }
+};
+
+const displayReviews = async (movieId) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/review/movie/${movieId}`);
+    displayReviewsDetails(response.data);
+  } catch (error) {
+    displayReviewsDetails(error.response.data);
+  }
+};
+
+const displayReviewsDetails = (reviews) => {
+  const reviewsDetailContainer = document.getElementById("reviews");
+  const isReviews = typeof reviews === "object";
+
+  reviewsDetailContainer.innerHTML = isReviews
+    ? reviews
+        .map(
+          (review) => `
+          <div class="card mb-3 bg-transparent text-white border" data-review-id="${
+            review.id
+          }">
+            <div class="card-body">
+            <div class="mb-3 d-flex flex-row justify-content-between">
+              <h5 class="card-title">${review.userId}</h5>
+              <div class="rating">${"⭐".repeat(review.rating)}</div>
+            </div>
+              <p class="card-text">${review.content}</p>
+              ${
+                review.userId &&
+                `<div class="d-flex justify-content-end">
+                    <button class="btn btn-sm btn-outline-primary me-2 edit-btn">
+                      <i class="bi bi-pencil"></i> 수정
+                    </button>
+                    <button class="btn btn-sm btn-outline-secondary delete-btn">
+                      <i class="bi bi-trash3"></i> 삭제
+                    </button>
+                  </div>`
+              }
+            </div>
+          </div>
+        `
+        )
+        .join("")
+    : `<p class="text-white">${reviews}</p>`;
+
+  const editButtons = reviewsDetailContainer.querySelectorAll(".edit-btn");
+  const deleteButtons = reviewsDetailContainer.querySelectorAll(".delete-btn");
+
+  editButtons.forEach((button) => {
+    const reviewId = button.closest(".card").getAttribute("data-review-id");
+    button.addEventListener("click", () => updateReview(reviewId));
+  });
+
+  deleteButtons.forEach((button) => {
+    const reviewId = button.closest(".card").getAttribute("data-review-id");
+    button.addEventListener("click", () => deleteReview(reviewId));
+  });
+};
+
+const updateReview = (reviewId) => {
+  const reviewCard = document.querySelector(`[data-review-id="${reviewId}"]`);
+  const reviewContent = reviewCard.querySelector(".card-text").textContent;
+  const reviewRating = reviewCard.querySelector(".rating").textContent.length;
+
+  reviewCard.innerHTML = `
+    <div class="card-body">
+      <div class="mb-3 d-flex flex-row justify-content-between">
+        <h5 class="card-title">리뷰 수정</h5>
+      </div>
+      <div class="mb-3">
+        <label class="form-label">별점</label>
+        <div class="col-md-6">
+          <div class="rating-card p-1">
+            <div class="star-rating animated-stars">
+              <input type="radio" id="star5" name="rating" value="5" 
+                ${reviewRating === 5 ? "checked" : ""} />
+              <label for="star5" class="bi bi-star-fill"></label>
+              <input type="radio" id="star4" name="rating" value="4" 
+                ${reviewRating === 4 ? "checked" : ""} />
+              <label for="star4" class="bi bi-star-fill"></label>
+              <input type="radio" id="star3" name="rating" value="3" 
+                ${reviewRating === 3 ? "checked" : ""} />
+              <label for="star3" class="bi bi-star-fill"></label>
+              <input type="radio" id="star2" name="rating" value="2" 
+                ${reviewRating === 2 ? "checked" : ""} />
+              <label for="star2" class="bi bi-star-fill"></label>    
+              <input type="radio" id="star1" name="rating" value="1" 
+                {reviewRating === 1 ? "checked" : ""} />
+              <label for="star1" class="bi bi-star-fill"></label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <label for="reviewContent" class="form-label">리뷰 내용</label>
+      <textarea id="updated-content" class="form-control mb-3">${reviewContent}</textarea>
+      <div class="d-flex justify-content-end">
+        <button class="btn btn-sm btn-outline-success me-2" onclick="submitUpdateReview('${reviewId}')">수정 완료</button>
+        <button class="btn btn-sm btn-outline-danger" onclick="cancelUpdateReview('${reviewId}')">취소</button>
+      </div>
+    </div>
+  `;
+};
+
+const submitUpdateReview = (reviewId) => {
+  const updatedContent = document.getElementById("updated-content").value;
+
+  const updatedRating = document.querySelector(
+    'input[name="rating"]:checked'
+  ).value;
+
+  updateReviewRequest(reviewId, updatedContent, updatedRating);
+};
+
+const cancelUpdateReview = (reviewId) => {
+  const reviewCard = document.querySelector(`[data-review-id="${reviewId}"]`);
+  displayReviews(movieId);
+};
+
+const updateReviewRequest = async (reviewId, content, rating) => {
+  try {
+    const response = await axios.patch(
+      `${BASE_URL}/review/${reviewId}`,
+      {
+        content: content,
+        rating: rating,
+      },
+      {
+        withCredentials: true,
+      }
+    );
+    alert(response.data);
+    displayReviews(movieId);
+  } catch (error) {
+    console.log(error.response.data);
+  }
+};
+
+const deleteReview = async (reviewId) => {
+  try {
+    const response = await axios.delete(`${BASE_URL}/review/${reviewId}`, {
+      withCredentials: true,
+    });
+    alert(response.data);
+    location.reload();
+  } catch (error) {
+    alert(error.response.data);
   }
 };
 
@@ -44,11 +193,11 @@ function displayMovieDetails(movie) {
 
   const hasPoster = movie.poster_path;
   const posterContent = hasPoster
-    ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="img-fluid rounded shadow" alt="${movie.title}">`
+    ? `<img src="${MOVIE_IMAGE_URL}${movie.poster_path}" class="img-fluid rounded shadow" alt="${movie.title}">`
     : `<div class="detail-poster">이미지 준비중입니다.</div>`;
 
   const backdropPath = movie.backdrop_path
-    ? `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`
+    ? `${MOVIE_IMAGE_URL}${movie.backdrop_path}`
     : null;
 
   if (backdropPath) {
@@ -100,6 +249,33 @@ function displayMovieDetails(movie) {
             </p>
         </div>
     `;
-
   document.title = `${movie.title} - MOVIETALK`;
 }
+
+const insertReview = async () => {
+  const reviewInput = document.getElementById("reviewContent");
+  const reviewContent = reviewInput.value;
+
+  try {
+    const response = await axios.post(
+      `${BASE_URL}/review`,
+      {
+        rating: rateValue,
+        content: reviewContent,
+      },
+      {
+        params: { movieId: movieId },
+        withCredentials: true,
+      }
+    );
+
+    alert(response.data);
+    location.reload();
+  } catch (error) {
+    alert(error.response.data);
+  }
+};
+
+const goToMyReviews = () => {
+  window.location.href = `/review.html?userId=${userId}`;
+};
